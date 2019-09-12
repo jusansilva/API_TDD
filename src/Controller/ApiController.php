65b9da04b873f03e205e75a04acaf9f669d7f77b
Jusan Silva <jusansilva>
2019-09-12 18:30:49 -0300

    atualização

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use Cake\Core\Configure;
+use Cake\Http\Exception\ForbiddenException;
+use Cake\Http\Exception\NotFoundException;
+use Cake\View\Exception\MissingTemplateException;
+
+class ApiControllers extends AppControllers{
+
+    public function index(){
+        return "";
+    }
+
+}
